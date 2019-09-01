dc3b25bec57d34d55971c58a603437dd70d8fd41
shainitenuwara <shainitenuwara@gmail.com>
2019-09-01 13:45:03 +0530

    change colors and add upadte and view..

---
+++ app/src/main/java/com/projects/naduni/eventplanner/ShoppingListUpdate.java
@@ -0,0 +1,13 @@
+package com.projects.naduni.eventplanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ShoppingListUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list_update);
+    }
+}
