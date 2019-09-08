4813b9bb060e579aba95db352957bb48c03c7ea0
inusha <inusham45@gmail.com>
2019-09-08 23:14:44 +0530

    Modified event insert and view,search

--- app/src/main/java/com/projects/naduni/eventplanner/CreateEvent.java
+++ app/src/main/java/com/projects/naduni/eventplanner/CreateEvent.java
@@ -2,14 +2,25 @@ package com.projects.naduni.eventplanner;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+import android.support.v4.app.Fragment;
+
+import com.projects.naduni.eventplanner.Model.Event;
+import com.projects.naduni.eventplanner.Service.DatabaseHelper;
 
 import java.util.Calendar;
 
@@ -18,22 +29,70 @@ import java.util.Calendar;
  * Created by inushaV on 8/31/2019.
  */
 
-public class CreateEvent extends AppCompatActivity implements
+public class CreateEvent extends Fragment implements
         DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
 
-    Button date_pick_button;
+    Button date_pick_button,event_add_button;
     TextView date_view;
+    EditText editTextEventName,editTextLocation,editTextNote,editTextDate;
 
     int day, month, year, hour, minute;
     int dayFinal, monthFinal, yearFinal, hourFinal, minuteFinal;
 
+    DatabaseHelper myDb;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_event);
+//        setContentView(R.layout.create_event);
 
-        date_pick_button = (Button) findViewById(R.id.button_pick_date);
-        date_view = (TextView) findViewById(R.id.edit_date);
+//        myDb = new DatabaseHelper(getActivity());
+//
+//        date_pick_button = (Button) findViewById(R.id.button_pick_date);
+//        event_add_button = (Button) findViewById(R.id.eventAddButton);
+//        date_view = (TextView) findViewById(R.id.edit_event_date);
+//
+//        editTextEventName = (EditText)findViewById(R.id.edit_event_name);
+//        editTextLocation = (EditText)findViewById(R.id.edit_event_location);
+//        editTextNote = (EditText)findViewById(R.id.edit_event_note);
+//
+//
+//        date_pick_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Calendar c = Calendar.getInstance();
+//                year = c.get(Calendar.YEAR);
+//                month = c.get(Calendar.MONTH);
+//                day = c.get(Calendar.DAY_OF_MONTH);
+//
+//                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), CreateEvent.this,
+//                        year, month, day);
+//                datePickerDialog.show();
+//            }
+//        });
+
+        //insert data to the Event table
+//        AddEventData();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        myDb = new DatabaseHelper(getActivity());
+
+        //Set title
+        getActivity().setTitle("Event Add");
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.create_event, container, false);
+
+        date_pick_button = (Button) view.findViewById(R.id.button_pick_date);
+        event_add_button = (Button) view.findViewById(R.id.eventAddButton);
+        date_view = (TextView) view.findViewById(R.id.edit_event_date);
+
+        editTextEventName = (EditText) view.findViewById(R.id.edit_event_name);
+        editTextLocation = (EditText) view.findViewById(R.id.edit_event_location);
+        editTextNote = (EditText) view.findViewById(R.id.edit_event_note);
+
 
         date_pick_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,12 +102,54 @@ public class CreateEvent extends AppCompatActivity implements
                 month = c.get(Calendar.MONTH);
                 day = c.get(Calendar.DAY_OF_MONTH);
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(CreateEvent.this, CreateEvent.this,
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), CreateEvent.this,
                         year, month, day);
                 datePickerDialog.show();
             }
         });
 
+        //insert data to the Event table
+        AddEventData();
+
+        return view;
+    }
+
+    public void AddEventData(){
+        event_add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            try {
+                Event event = new Event();
+                event.setEventName(editTextEventName.getText().toString());
+                event.setLocation(editTextLocation.getText().toString());
+                event.setDate(date_view.getText().toString());
+                event.setNote(editTextNote.getText().toString());
+
+                //Print data in the console
+                System.out.println("Insert Event Data = " + editTextEventName.getText().toString() +","+ editTextLocation.getText().toString() +","+  date_view.getText().toString() +","+ editTextNote.getText().toString());
+
+                boolean isInserted = myDb.insertEventData(event);
+                if (isInserted = true)
+                    Toast.makeText(getActivity(),"Data Inserted Successfully..",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getActivity(),"Data Inserted Error ..",Toast.LENGTH_LONG).show();
+
+                openEventFregment();
+
+
+            } catch (Exception e) {
+                System.out.println("Error " + e.getMessage());
+            }
+
+            }
+        });
+    }
+
+    public void openEventFregment(){
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.content_frame, new EventFragment());
+        ft.commit();
     }
 
 
@@ -62,8 +163,8 @@ public class CreateEvent extends AppCompatActivity implements
         hour = c.get(Calendar.HOUR_OF_DAY);
         minute = c.get(Calendar.MINUTE);
 
-        TimePickerDialog timePickerDialog = new TimePickerDialog(CreateEvent.this, CreateEvent.this,
-                hour, minute, DateFormat.is24HourFormat(this));
+        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), CreateEvent.this,
+                hour, minute, DateFormat.is24HourFormat(getActivity()));
         timePickerDialog.show();
     }
 
--- app/src/main/java/com/projects/naduni/eventplanner/Service/DatabaseHelper.java
+++ app/src/main/java/com/projects/naduni/eventplanner/Service/DatabaseHelper.java
@@ -6,13 +6,25 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.projects.naduni.eventplanner.Model.Event;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME ="eventplanner.db";
     public static final String TABLE_NAME ="todolist";
+    public static final String TABLE_EVENT = "events";
+
+    //common column names
     public static final String COL_1 ="ID";
+
     public static final String COL_2 ="NAME";
     public static final String COL_3 ="NOTE";
+
+    //Event table column names
+    public static final String COL_EVENT_NAME = "EVENTNAME";
+    public static final String COL_EVENT_LOCATION = "EVENTLOCATION";
+    public static final String COL_EVENT_DATE = "EVENTDATE";
+    public static final String COL_EVENT_NOTE = "EVENTNOTE";
 
     public DatabaseHelper(Context context) {
         super(context,DATABASE_NAME,null,1);
@@ -21,20 +33,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table "+ TABLE_NAME +"(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,NOTE TEXT)");
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table "+ TABLE_NAME +"(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,NOTE TEXT)");
+        db.execSQL("create table " + TABLE_EVENT +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,EVENTNAME TEXT,EVENTLOCATION TEXT,EVENTDATE TEXT,EVENTNOTE TEXT)");
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
-        onCreate(sqLiteDatabase);
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_EVENT);
+        onCreate(db);
     }
 
-
     public boolean insertData(String task, String note){
-
 
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -49,12 +61,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+    //insert Event details
+    public boolean insertEventData(Event event){
+        System.out.println("Error insertEventData " + event.getEventName());
+        SQLiteDatabase db = this.getReadableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_EVENT_NAME,event.getEventName());
+        contentValues.put(COL_EVENT_LOCATION,event.getLocation());
+        contentValues.put(COL_EVENT_DATE,event.getDate());
+        contentValues.put(COL_EVENT_NOTE,event.getNote());
+
+        long result = db.insert(TABLE_EVENT,null ,contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
+
     public Cursor viewAllData(){
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor res = db.rawQuery("select * from "+ TABLE_NAME ,null);
         return res;
     }
-
-
-
 }
